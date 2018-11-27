97084e48f57da97ac66493758540ca71392decb7
Andy Tudhope <tuddy0525@gmail.com>
2018-11-27 10:55:44 +0200

    Adds support for sending tokens

--- src/App.js
+++ src/App.js
@@ -8,11 +8,40 @@ import Web3 from "web3";
 
 const donationNetworkID = 1; // make sure donations only go through on this network.
 
-const donationAddress = "0xf7050c2908b6c1ccdfb2a44b87853bcc3345e3b3"; //replace with the address to watch
+const donationAddress = "0xf7050c2908b6c1ccdfb2a44b87853bcc3345e3b3";
+
+const SNTaddress = '0x744d70FDBE2Ba4CF95131626614a1763DF805B9E';
+const DAIaddress = '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359';
+const minABI = [
+  // transfer
+  {
+   "constant": false,
+   "inputs": [
+    {
+     "name": "_to",
+     "type": "address"
+    },
+    {
+     "name": "_value",
+     "type": "uint256"
+    }
+   ],
+   "name": "transfer",
+   "outputs": [
+    {
+     "name": "",
+     "type": "bool"
+    }
+   ],
+   "type": "function"
+  }
+ ];
 
 var typeNumber = 4;
 var errorCorrectionLevel = 'L';
 var qr = qrcode(typeNumber, errorCorrectionLevel);
+
+let tokenAddress;
 
 class App extends Component {
   constructor(props) {
@@ -75,6 +104,47 @@ class App extends Component {
     });
   };
 
+  handleTokenDonate = event => {
+    event.preventDefault();
+    const form = document.getElementById('token-form');
+    let donateTokens = form.elements["token_amount"].value;
+    // Use BigNumber
+    let decimals = web3.utils.toBN(18);
+    let amount = web3.utils.toBN(donateTokens);
+
+    // Select correct contract address
+    if (form.elements.token.value === 'snt') {
+      tokenAddress = SNTaddress;
+    } else if (form.elements.token.value === 'dai') {
+      tokenAddress = DAIaddress;
+    } else {
+      alert("No valid token selected")
+    }
+
+    window.web3.eth.net.getId().then(netId => {
+      if (netId === donationNetworkID) { 
+        return window.web3.eth.getAccounts().then(accounts => {
+          // Get ERC20 Token contract instance
+          let contract = new window.web3.eth.Contract(minABI, tokenAddress);
+          // calculate ERC20 token amount
+          let value = amount.mul(web3.utils.toBN(10).pow(decimals));
+          // call transfer function
+          return contract.methods.transfer(donationAddress, value).send({from: accounts[0]})
+            .on('transactionHash', function(hash){
+              console.log(hash);
+            });
+          }).catch(e => {
+            alert(e);
+        });
+      } else {
+        alert("You cannot make a donation on this network");
+        this.setState({
+          donateenabled: false
+        });
+      }
+    })
+  }
+
   componentDidMount = () => {
     if (window.ethereum) {
       this.setState({
@@ -120,6 +190,19 @@ class App extends Component {
                   />
                   <input type="text" placeholder="message" name="message" />
                   <button onClick={this.handleDonate} className="donation-button">Donate Now!</button>
+                  </form>
+                  <div className="clear"></div>
+                  <form id="token-form">
+                  <input
+                    type="text"
+                    placeholder="Tokens to donate"
+                    name="token_amount"
+                  />
+                  <select className="token-select" name="token">
+                    <option value="snt">SNT</option>
+                    <option value="dai">DAI</option>
+                  </select>
+                  <button onClick={this.handleTokenDonate} className="donation-button">Donate Tokens</button>
                 </form>
               </div>
               ) : (
